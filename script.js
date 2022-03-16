bd9f631789a0a0555af340c32b659b21f02778ee
dom b <bellybuchelli1234@gmail.com>
2022-03-16 11:13:37 -0400

    'commit'

---
+++ script.js
@@ -0,0 +1,10 @@
+function loginlogout(Element){
+    Element.innerText = "Logout"
+}
+function hide(element){
+    element.remove();
+}
+// function ilike(){
+//     var x= "Ninja was liked"
+//     console.log(x)
+// }nevermind
